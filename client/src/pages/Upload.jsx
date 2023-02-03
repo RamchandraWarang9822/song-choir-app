9313cf760c8d2411350658233cdca05366fe5d93
Ramchandra Warang <92023869+RamchandraWarang9822@users.noreply.github.com>
2023-02-03 17:57:24 +0530

    delete unncessary imports

--- client/src/pages/Upload.jsx
+++ client/src/pages/Upload.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FormField, Loader } from '../components';
 import axios from "axios";
-import { useEffect } from 'react';
 
 const Upload = () => {
 
